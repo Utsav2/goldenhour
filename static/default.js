6a189172eaa7b3cbc275df3a081f6a8fb065db0c
Utsav Shah <ukshah2@illinois.edu>
2014-06-30 11:47:30 +0530

    trying old flare

---
+++ static/default.js
@@ -0,0 +1,123 @@
+var app = angular.module("menu", []);
+
+app.config(function($interpolateProvider) {
+  $interpolateProvider.startSymbol('{[{');
+  $interpolateProvider.endSymbol('}]}');
+});
+
+window.onload = function (){
+
+	createGoogleMap();
+
+}
+
+function createGoogleMap() {
+
+	var stylesArray = [{
+
+	    "featureType": "landscape",
+	    "stylers": [{
+	        "saturation": -100
+	    }, {
+	        "lightness": 65
+	    }, {
+	        "visibility": "on"
+	    }]
+	}, {
+	    "featureType": "poi",
+	    "stylers": [{
+	        "saturation": -100
+	    }, {
+	        "lightness": 51
+	    }, {
+	        "visibility": "simplified"
+	    }]
+	}, {
+	    "featureType": "road.highway",
+	    "stylers": [{
+	        "saturation": -100
+	    }, {
+	        "visibility": "simplified"
+	    }]
+	}, {
+	    "featureType": "road.arterial",
+	    "stylers": [{
+	        "saturation": -100
+	    }, {
+	        "lightness": 30
+	    }, {
+	        "visibility": "on"
+	    }]
+	}, {
+	    "featureType": "road.local",
+	    "stylers": [{
+	        "saturation": -100
+	    }, {
+	        "lightness": 40
+	    }, {
+	        "visibility": "on"
+	    }]
+	}, {
+	    "featureType": "transit",
+	    "stylers": [{
+	        "saturation": -100
+	    }, {
+	        "visibility": "simplified"
+	    }]
+	}, {
+	    "featureType": "administrative.province",
+	    "stylers": [{
+	        "visibility": "off"
+	    }]
+	}, {
+	    "featureType": "water",
+	    "elementType": "labels",
+	    "stylers": [{
+	        "visibility": "on"
+	    }, {
+	        "lightness": -25
+	    }, {
+	        "saturation": -100
+	    }]
+	}, {
+	    "featureType": "water",
+	    "elementType": "geometry",
+	    "stylers": [{
+	        "hue": "#ffff00"
+	    }, {
+	        "lightness": -25
+	    }, {
+	        "saturation": -97
+	    }]
+	}]
+
+	var mapOptions = {
+
+        center: new google.maps.LatLng(18.397, 72.644),
+        zoom: 13,
+        mapTypeId: google.maps.MapTypeId.HYBRID
+
+    };
+
+	var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+	map.setOptions({styles: stylesArray});
+}
+
+app.directive("enter", function(){
+ return function(scope, element) {
+      element.bind("mouseenter", function(){
+        
+      	
+        
+      })
+    }
+});
+
+app.directive("leave", function(){
+ return function(scope, element) {
+      element.bind("mouseleave", function(){
+        
+      })
+    }
+});
