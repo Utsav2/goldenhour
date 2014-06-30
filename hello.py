3cabc9cdee778f972900c1b8fa0cae4abc4a1063
Utsav Shah <ukshah2@illinois.edu>
2014-06-30 12:08:22 +0530

    trying old flare

--- hello.py
+++ hello.py
@@ -1,9 +1,12 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 import jinja2
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return render_template("index.html", title = 'Home')
+
+if __name__ == "__main__":
+    app.run(debug = True)
