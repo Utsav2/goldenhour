4ebba7038fac0cc48294f1300f2d9aef2ab9f5c1
Utsav Shah <ukshah2@illinois.edu>
2014-06-30 12:14:07 +0530

    old flare deployed, now tryin db

--- hello.py
+++ hello.py
@@ -1,8 +1,11 @@
 import os
 from flask import Flask, render_template
+from flask.ext.sqlalchemy import SQLAlchemy
 import jinja2
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL', 'postgres://avodddfdxytrat:yFn8_7fiQEdlhkhPJ0UjsQukCJ@ec2-54-225-135-30.compute-1.amazonaws.com:5432/df09oj774bls87']
+db = SQLAlchemy(app)
 
 @app.route('/')
 def hello():
