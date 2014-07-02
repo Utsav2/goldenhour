ebf06922b5f96b7701dddbdcbfe0afa17f922afc
Utsav Shah <ukshah2@illinois.edu>
2014-07-02 16:18:02 +0530

    trying internet database connection

--- hello.py
+++ hello.py
@@ -85,7 +85,7 @@ def upload():
         area = address["Administrative Area"]
         locality = address["Locality"]
         # picture_url = request.values['image']
-        #report = Report(type_request, imei, latitude, longitude, description, number, time, country, area, locality)
+        report = Report(type_request, imei, latitude, longitude, description, number, time, country, area, locality)
         #db.session.add(report)
         #db.session.commit()
 
