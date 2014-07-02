aff86ac9f9971d4f2000e17eb6ef01cbabdc8897
Utsav Shah <ukshah2@illinois.edu>
2014-07-03 00:01:56 +0530

    JFJ

--- hello.py
+++ hello.py
@@ -74,7 +74,8 @@ def upload():
         country = address["Country"]
         area = address["Administrative Area"]
         locality = address["Locality"]
-        picture_url = request.files['image']
+        try:
+            picture_url = request.files.get('image')
         report = Report(type_request, imei, latitude, longitude, description, number, time, country, area, locality)
         db.session.add(report)
         db.session.commit()
