0a2b5fc7dff57c832a14b7107bcd61d7c1a84664
Utsav2 <ukshah2@illinois.edu>
2014-10-24 22:21:20 -0500

    Hello

--- hello.py
+++ hello.py
@@ -1,9 +1,6 @@
 import os
 from flask import Flask, render_template, request, jsonify, send_from_directory 
 import jinja2
-import json
-import hashlib
-from werkzeug import secure_filename
 
 
 app = Flask(__name__)
@@ -11,7 +8,7 @@ app = Flask(__name__)
 @app.route("/")
 def main_page():
 	return render_template('index.html')
-	
+
 if __name__ == "__main__":
     app.run(debug = True)
 
