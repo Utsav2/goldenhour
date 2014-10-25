d3e52254de11bef328d7d31d712c37261b5a1b98
Utsav2 <ukshah2@illinois.edu>
2014-10-24 22:23:46 -0500

    Hello

--- hello.py
+++ hello.py
@@ -2,7 +2,6 @@ import os
 from flask import Flask, render_template, request, jsonify, send_from_directory 
 import jinja2
 
-
 app = Flask(__name__)
 
 @app.route("/")
@@ -10,5 +9,7 @@ def main_page():
 	return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(debug = True)
+	port = int(os.environ.get("PORT", 5000))
+	app.run(debug=True, host='0.0.0.0', port=port)
+    #app.run(debug = True)
 
